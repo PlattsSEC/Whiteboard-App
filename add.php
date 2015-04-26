e6bc7e28b40ce8ae7f7eda2c8b25e20a84f99b6a
Tyler Legge <tylerlegge@Tylers-MacBook-Pro-3.local>
2015-04-26 19:55:57 -0400

    add.php

---
+++ add.php
@@ -0,0 +1,37 @@
+<?php
+   class MyDB extends SQLite3
+   {
+      function __construct()
+      {
+         $this->open('/Users/tylerlegge/sqlite3_data/test.db');
+      }
+   }
+   $db = new MyDB();
+   if(!$db){
+      echo $db->lastErrorMsg();
+   } else {
+      echo "Opened database successfully\n";
+   }
+
+   $sql =<<<EOF
+      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+      VALUES (1, 'Paul', 32, 'California', 20000.00 );
+
+      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+      VALUES (2, 'Allen', 25, 'Texas', 15000.00 );
+
+      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+      VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );
+
+      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+      VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );
+EOF;
+   header('Content-type: text/plain');
+   $ret = $db->exec($sql);
+   if(!$ret){
+      echo $db->lastErrorMsg();
+   } else {
+      echo "Records created successfully\n";
+   }
+   $db->close();
+?>
---
+++ select.php
@@ -0,0 +1,29 @@
+<?php
+   class MyDB extends SQLite3
+   {
+      function __construct()
+      {
+         $this->open('/Users/tylerlegge/sqlite3_data/test.db');
+      }
+   }
+   $db = new MyDB();
+   if(!$db){
+      echo $db->lastErrorMsg();
+   } else {
+      echo "Opened database successfully\n";
+   }
+
+   $sql =<<<EOF
+      SELECT * from COMPANY;
+EOF;
+
+   $ret = $db->query($sql);
+   while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+      echo "ID = ". $row['ID'] . "\n";
+      echo "NAME = ". $row['NAME'] ."\n";
+      echo "ADDRESS = ". $row['ADDRESS'] ."\n";
+      echo "SALARY =  ".$row['SALARY'] ."\n\n";
+   }
+   echo "Operation done successfully\n";
+   $db->close();
+?>
